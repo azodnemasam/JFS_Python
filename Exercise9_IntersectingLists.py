7256464a81da293e93de39fe1e181c5c4475f872
azodnemasam <35142894+azodnemasam@users.noreply.github.com>
2018-03-14 20:23:02 +0800

    Update Exercise9_IntersectingLists.py

--- Exercise9_IntersectingLists.py
+++ Exercise9_IntersectingLists.py
@@ -6,8 +6,7 @@ L2 = map(int,input('Enter numbers separated by comma for List2:').split(','))
 L2 = set([int(x) for x in L2])
 
 def intersectingElements(L1,L2):
-    elem_list = list(L1&L2)
-    result = {'intersecting_elements':elem_list,'count':len(elem_list)}
+    result = {'intersecting_elements':list(L1&L2),'count':len(elem_list)}
     return result
 
 print(intersectingElements(L1,L2))
